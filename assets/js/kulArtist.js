30ed0948d8a648a21800cb62a8c0cd9408e0fd4b
Pintu <pintu.chaudhary9@gmail.com>
2020-05-22 11:12:16 +0100

    My Portfolio Final

---
+++ assets/js/kulArtist.js
@@ -0,0 +1,415 @@
+//Object of Projects details for modal
+var projectDetails=[
+{
+	id:"0",
+	name:"Yelp Camp",
+	heading:"Website | Javascript, NodeJs, ExpressJs, MongoDb, AWS Cloud9, NPM, Mongoose",
+	description:"Responsive web design to display list of campgrounds to the users <br>User authentication (Login/Register/Logout) using passport tool <br> User authorization for adding/updating/deleting campground or comments <br>Restful routing according to REST standards <br>AWS Cloud9 IDE along with MongoDb for development and data storage <br>Backend implementation with expressJs, NodeJs and mongoose.",
+	demo:"https://gilpix.github.io/My-Portfolio/",
+	code:"https://github.com/pintujat/YelpCamp",
+	image:"yelpcamp.png"
+ },
+ {
+ 	id:"1",
+	name:"RESTful Blog",
+	heading:"Website : Javascript, NodeJs, ExpressJs, Sementic UI, MongoDb, AWS Cloud9",
+	description:"A RESTful routes blog for creating, reading, updating and deleting blog post <br>Responsive web design using semantic ui <br>AWS Cloud9 IDE and MongoDb for development and data storage. ",
+	demo:"https://gilpix.github.io/My-Portfolio/",
+	code:"https://github.com/pintujat/RESTFulBlog",
+	image:"RestFul Blog.png"
+ },
+{
+	id:"2",
+	name:"My Portfolio",
+	heading:"Pintu Jat Software Developer Portfolio Website",
+	description:"pintujat is an software developer portfolio static website for me(Pintu Jat). Designed and written in html, css and javascript. Used two famous librearies <strong>Bootstrap  4.0</strong> and <strong>jQuery</strong> for more effects and designing.",
+	demo:"https://pintujat.github.io/MyPortfolio/",
+	code:"https://github.com/pintujat/MyPortfolio",
+	image:"myportfolio.png"
+ },
+ {
+	id:"3",
+	name:"Tekhub",
+	heading:"Website : TekHub | Html, Css, Javascript, jQuery, Bootstrap 4",
+	description:"It is a front-end design of product lending system  used by the students of CEGEP de la gaspesie to borrow devices  like laptops, lan cables, etc. from the college.<br>Responsive web design for students to borrow item from college <br> Forms(signIn, signUp) validation <br> jQuery and bootstrap libraries included for better design and effects. ",
+	demo:"https://pintujat.github.io/TekHub-Lending-Web-Application/",
+	code:"https://github.com/pintujat/TekHub-Lending-Web-Application",
+	image:"tekhubweb.png"
+ },
+ {
+	id:"4",
+	name:"Tekhub",
+	heading:"Android Lending App : TekHub | Android, Java, WebApi, AmazonRDS MySql",
+	description:"TekHub application is  used by the students of Lambton College to borrow devices  like laptops, lan cables, etc. from the college.<br>Achieved login validation, registration of user and admin<br>Web calls for data exchange between app and AmazonRDS Mysql database<br>Database connectivity between AmazonRDS - Netbeans(webcall) - Android<br>Added features to add item in waiting list, borrow items, filter items list, give feedback/ratings and updatethe database accordingly.<br>Used Agile methodology-SCRUM to develop the app and created SRS documentation. ",
+	demo:"https://github.com/Gilpix/TekHub",
+	code:"https://github.com/Gilpix/TekHub",
+	image:"tekhubmobile.png"
+ },
+ {
+	id:"5",
+	name:"Food Buddy",
+	heading:"Android App : FoodBuddy | Android, Java, Firebase, Xampp, MySql",
+	description:"The FoodBuddy application will provide its user the ability to select a restaurant and search for a buddy/partner who can join him/her for food.<br>Developed new user registration along with reset password functionality.<br> Displayed list of available users in recycler view from mysql db using xampp server and phpMyadmin web application. <br>Send invitation(notification) to other user using Firebase Cloud Messaging(FCM) service. <br> Developed SRS document for app development. <br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/foodbuddySrs.pdf' download>SRS-Document</a>",
+	demo:"https://github.com/Gilpix/Food-Buddy",
+	code:"https://github.com/Gilpix/Food-Buddy",
+	image:"foodbuddy.png"
+ },
+ {
+	id:"6",
+	name:"Car Market",
+	heading:"Website design - html | css",
+	description:"Car Market is a front-end design of car selling web application.<br>Fully designed website includes home page, contact page, signIn/signUp page and car display page.",
+	demo:"https://gilpix.github.io/CarMarket/index.html",
+	code:"https://github.com/Gilpix/CarMarket",
+	image:"carmarket.png"
+ },
+ {
+	id:"7",
+	name:"Traffic Road Sign Canada",
+	heading:"Android Traffic Road Sign Canada App | Android, Google Play+Console",
+	description:"It is a theory and quiz based android app for users to prepare for Canadian driving test. <br> Uploaded on Google play store for better understanding of the insights of app and users using Google developer play console. ",
+	demo:"https://play.google.com/store/apps/details?id=com.vid_lancer.trafficroadsignscanada",
+	code:"https://github.com/Gilpix/TrafficRoadSignsCanada",
+	image:"traffic.png"
+ },
+ {
+	id:"8",
+	name:"Color Guessing Game",
+	heading:"Web Application | javascript, css, bootstrap4, html",
+	description:"Web application using Javascript CSS HTML - Wher user can select any color square from the given squares depending on the RGB color value shown. If user guesses right color square, user wins else the selected square will dissappear and now user can guess from the remaining squares. Ther will be two mode easy mode with just there squares and hard mode with six squares.",
+	demo:"https://gilpix.github.io/ColorGuessingGame/",
+	code:"https://github.com/Gilpix/ColorGuessingGame",
+	image:"colorguess.png"
+ },{
+	id:"9",
+	name:"To Do List",
+	heading:"Web Application | jQuery, CSS, HTML",
+	description:"Resposive web application ToDoList using jQuery, CSS and HTML, where user can add item to list, remove item from list or mark completed items.",
+	demo:"https://gilpix.github.io/ToDoList/",
+	code:"https://github.com/Gilpix/ToDoList",
+	image:"todolist.png"
+ },
+ {
+	id:"10",
+	name:"Travel With Us",
+	heading:"Android App Design | Prototype : JustInmind software",
+	description:"Travel With Us is an android application screens design and prototype for bus booking system.<br>It is benificial for two type of travellers Intercity and Intracity. User can easily book tickets and can also cancel them if needed.<br>Intercity Bus Booking : In this category user can use this app to travel within the city.<br>Intracity Bus Booking : In this category user can use this app to travel from one city to another. ",
+	demo:"https://gilpix.github.io/TekHub-Lending-Web-Application/",
+	code:"https://github.com/Gilpix/TekHub-Lending-Web-Application",
+	image:"travel.png"
+ },
+ {
+	id:"11",
+	name:"Share Expenses",
+	heading:"Android App Design | Prototype : Justinmind software",
+	description:"Share Expenses is an android application design which is used to calculate expenses for single user and split expenses between two or more users.<br><strong>Features :</strong><br>Calculate weekly/monthly/yearly expenses of single user<br>Split expenses between users<br>Remind other users to clear out their dues<br>Receive notification for any updation/creation/deletion of bill<br>Add new friends or create new groups easily<br>Can add bills when offline  <br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/Share Expense ScreenFlowDiagram.pdf' download>Screen-Flow</a>",
+	demo:"https://gilpix.github.io/TekHub-Lending-Web-Application/",
+	image:"shareexpenses.png"
+ },
+ {
+	id:"12",
+	name:"Pro Quiz",
+	heading:"Android Quiz application | Android programming, xml, google play store",
+	description:"Pro Quiz is a free game to let you enjoy guessing lots of question, picture and audio from different categories like sports, logos, music, nature, monuments etc. Pro Quiz is a simple game based on the users general knowledge and on the basis of user knowledge, user is divided into for groups Poor, Average, Good and Excellent.",
+	demo:"https://play.google.com/store/apps/details?id=com.gilpix.test",
+	code:"https://github.com/Gilpix/TekHub-Lending-Web-Application",
+	image:"proquiz.png"
+
+ }
+];
+
+var tekHubResource="<br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/LendSystemIteration3.pdf' download>SRS-Document</a>  |  <a class='modal-res-links' target='_blank' href='assets/res/TekhubFinalPresentation.pdf' download>Presentation</a>";
+
+
+//Get value for selected project from ProjectDetails object
+function showModal(no){
+	$(".card-button").click(function(){
+		var id=projectDetails[no].id;
+		var name =projectDetails[no].name;
+		var shortDesc =projectDetails[no].heading;
+		var description =projectDetails[no].description;
+		var demo=projectDetails[no].demo;
+		var code=projectDetails[no].code;
+		var image =projectDetails[no].image;
+		modal(id,name,shortDesc, description,demo,code,image);
+	})
+}
+
+//Show Modal for selected project
+function modal(id,name,shortDesc, description,demo,code,image){
+	$(".modal-heading").text(name);
+	$(".modal-short-text").text(shortDesc);
+	$(".modal-desc-text").html(description);
+	$(".project-demo").attr("href",demo);
+	$(".project-code").attr("href",code);
+	$(".modal-image").attr("src","assets/res/"+image);
+	if(id==11 || id==10 || id==9 || id==4 || id==6 || id==8)
+	{
+		$(".project-code").addClass("hide-cards");
+		$(".project-demo").addClass("hide-cards");
+	}
+	else if(id==5 || id==0 || id==1 )
+	{
+		$(".project-demo").addClass("hide-cards");
+	}
+	else if(id==12 || id==7 || id==4)
+	{
+		$(".project-code").addClass("hide-cards");
+	}
+	else if (id==12) 
+	{
+		$(".project-demo").addClass("hide-cards");
+	}
+	else
+	{
+		$(".project-code").removeClass("hide-cards");
+		$(".project-demo").removeClass("hide-cards");
+	}
+	$(".myModal").modal('show');
+}
+
+
+//Object of Projects Card details
+var projectInfo=[
+
+	{
+		id:"0",
+		title:"Yelp Camp",
+		languages:"mongoDb/expressJs/nodeJs",
+		category:"web",
+		icon:"<i class='fa fa-2x fa-campground'></i><br>",
+		cardImage:"card-img-web"
+	},
+	{
+		id:"1",
+		title:"RESTful Blog",
+		languages:"mongoDb/expressJs/nodeJs",
+		category:"web",
+		icon:"<i class='fab fa-2x fa-blogger'></i><br>",
+		cardImage:"card-img-web"
+	},
+	{
+		id:"2",
+		title:"My Portfolio",
+		languages:"html/css/jQuery",
+		category:"web",
+		icon:"<i class='far fa-2x fa-address-card'></i><br>",
+		cardImage:"card-img-web"
+	},
+	{
+		id:"3",
+		title:"TekHub",
+		languages:"html/css/javascript",
+		category:"web",
+		icon:"<i class='fas fa-2x fa-shopping-basket'></i><br>",
+		cardImage:"card-img-web"
+	},
+	{
+		id:"4",
+		title:"TekHub",
+		languages:"java/android/mysql",
+		category:"mobile",
+		icon:"<i class='fas fa-2x fa-utensils'></i><br>",
+		cardImage:"card-img-mobile"
+	},
+	{
+		id:"5",
+		title:"Food Buddy",
+		languages:"android/xml/mysql",
+		category:"mobile",
+		icon:"<i class='fas fa-2x fa-shopping-basket'></i><br>",
+		cardImage:"card-img-mobile"
+	},
+	
+	{
+		id:"6",
+		title:"Car Market",
+		languages:"html/css",
+		category:"web",
+		icon:"<i class='fas fa-2x fa-car-side'></i><br>",
+		cardImage:"card-img-web"
+	},
+	{
+		id:"7",
+		title:"Traffic Road Sign",
+		languages:"java/android/xml",
+		category:"mobile",
+		icon:"<i class='fas fa-2x fa-traffic-light'></i><br>",
+		cardImage:"card-img-mobile"
+	},
+	{
+		id:"8",
+		title:"Color Guess",
+		languages:"html/css/javascript",
+		category:"web",
+		icon:"<i class='fas fa-2x fa-gamepad'></i><br>",
+		cardImage:"card-img-web"
+	},
+	{
+		id:"9",
+		title:"ToDo List",
+		languages:"jQuery/css/html",
+		category:"web",
+		icon:"<i class='fas fa-2x fa-list-ul'></i><br>",
+		cardImage:"card-img-web"
+	},
+	{
+		id:"10",
+		title:"Travel with us",
+		languages:"justinmind/design/prototype",
+		category:"other",
+		icon:"<i class='fas fa-2x fa-bus'></i><br>",
+		cardImage:"card-img-others"
+	},
+	{
+		id:"11",
+		title:"Share Expenses",
+		languages:"justinmind/ui design",
+		category:"other",
+		icon:"<i class='fas fa-2x fa-hand-holding-usd'></i><br>",
+		cardImage:"card-img-others"
+	},
+	{
+		id:"12",
+		title:"Pro Quiz",
+		languages:"java/android/play-store",
+		category:"mobile",
+		icon:"<i class='far fa-2x fa-question-circle'></i><br>",
+		cardImage:"card-img-mobile"
+	}
+];
+
+//create card for each project dynamically
+	$.each(projectInfo,function(i){
+	 var temp = "<div class='"+projectInfo[i].category+" all col-sm-12 col-md-6 col-lg-4 px-2 py-2'> <div class='card "+projectInfo[i].cardImage+"'>"
+      +"<p class='text-center project-title'>"+projectInfo[i].icon+projectInfo[i].title+"</p>"
+      +"<div id='card3' class='card-img-overlay pt-5'> <div class='text text-center text-content-card'>"
+    +"<p class='card-titlee'>"+projectInfo[i].title+"</p>"
+    +"<p class='card-texte mb-5'>"+projectInfo[i].languages+"</p>"
+     +" <a href='#/' class='card-button' onclick='showModal("+i+")' >Learn More</a>" +"</div>"+"</div>"+"</div>" 
+	   
+	    $("#myProject").append(temp);
+
+	    $(".card-button").on("click", function () 
+	    {
+			showModal(i);
+		});
+	});
+
+
+//Navbar navigation control
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//Add active class to current Navigation Link
+ $("#navbar a").click(function(){
+ 	$("a").removeClass("active");
+ 	$(this).addClass("active");
+ });
+  $(".all-projects-container a div").click(function(){
+ 	$("div").removeClass("active-projects");
+ 	$(this).addClass("active-projects");
+ });
+
+
+
+  $(document).ready(function(){
+
+//Move Profile Image horizontally	
+ $("#profile-image").click(function(){
+ 	$("#profile-image").effect( "shake", {times:2,distance:180}, 8000 );
+ });
+
+//Show and hide content of Home Page using effects
+$( "#start" ).click(function() {
+	// $("#cont").slideDown(1000);
+  $( "#cont" ).toggle( "fade", { times: 3 }, "slow",function(){
+  	$( "#cont2" ).delay(200).toggle( "fade", { times: 3 }, "slow" );
+  	$( "#cont3" ).delay(1000).toggle( "drop", "slow" );
+
+  } );
+});
+
+//Move Profile Image Vertically
+ $('#let').click(function(){
+  	// $("#profile-image").stop(true, true);
+  	// $( "#profile-image" ).animate({ bottom: "-=10px" }, 2000 );
+$( "#profile-image" ).effect( "bounce",{times:4,distance:90},8000 );
+});
+ });
+
+
+
+ //Filter Projects 
+$("#all-projects").click(function(){
+	$(".web").removeClass("hide-cards");
+	$(".mobile").removeClass("hide-cards");
+	$(".other").removeClass("hide-cards");
+	});
+
+$("#web-projects").click(function(){
+	$(".web").removeClass("hide-cards");
+	$(".mobile").addClass("hide-cards");
+	$(".other").addClass("hide-cards");
+	});
+
+$("#mobile-projects").click(function(){
+	$(".mobile").removeClass("hide-cards");
+	$(".web").addClass("hide-cards");
+	$(".other").addClass("hide-cards");
+	});
+
+$("#other-projects").click(function(){
+	$(".other").removeClass("hide-cards");
+	$(".mobile").addClass("hide-cards");
+	$(".web").addClass("hide-cards");
+	});
+
+$('modal').on('shown.bs.modal', function () {
+  $('.modal').trigger('focus')
+})
+
+
+//hide project overlay and show lear more button with effect
+ $(".card-img-overlay").each( function()
+ {
+ 	$(this).hover(function(){
+ 	$(this).addClass("card-background");
+ 	$(this).find(".text").removeClass("text-content-card");
+ 	},
+ 	function()
+ 	{	$(this).removeClass("card-background");
+ 		$(this).find(".text").addClass("text-content-card");
+ 	}
+ 	);
+ });
+
+ $(".card-img-overlay").each( function()
+ {
+	$(this).hover(function()
+	{
+   		$(this).find(".card-titlee").slideDown( 'slow', function()
+   		{ 
+   			$(this).addClass("card-background");
+ 			$(this).find(".text").removeClass("text-content-card");
+  	 });
+   	},
+  	 function()
+	  {
+	   $(this).find(".card-titlee").slideUp( 'slow', function()
+	   { 
+	   	 $(this).removeClass("card-background");
+	 	 $(this).find(".text").removeClass("text-content-card");
+	   });
+});
+ });
